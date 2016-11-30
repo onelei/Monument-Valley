d7fb36f02792b5b68586513fee5a1c41873bfea1
Lei Wang <ahleiwolong@163.com>
2016-11-30 11:04:28 +0800

    test

---
+++ Monument-Valley/Assets/Define.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Define
+{
+    // some colors;
+    public static Color Green = new Color(0f, 1f, 0.29f);
+}
+
---
+++ Monument-Valley/Assets/UIMain.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIMain : MonoBehaviour {
+
+	MapMaker myScenece;
+
+	// Use this for initialization
+	void Start () {
+		myScenece = GameObject.Find ("MapMaker").GetComponent<MapMaker> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		GUI.Box(new Rect(10,10,100,180), "Scenes");
+		
+		if(GUI.Button(new Rect(20,40,80,20), "Scene 1")) {
+			myScenece.Load (1);
+		}
+		 
+	}
+}
